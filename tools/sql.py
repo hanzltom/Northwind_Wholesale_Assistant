717e6986fdb63c5331e6b3294a67fe8e88ad133e
Tomas Hanzlik <tomhanzlik1@gmail.com>
2026-07-23 22:19:54 +0200

    sql.py: 3 tools

--- tools/sql.py
+++ tools/sql.py
@@ -6,19 +6,19 @@ from langchain.tools import tool
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / 'data' / 'northwind.db'
-DB_URI = f"file:{DB_PATH}?mode=ro"
+DB_URI_RO = f"file:{DB_PATH}?mode=ro"
+DB_URI_RW = f"file:{DB_PATH}?mode=rw"
 
-def _create_connection() -> sqlite3.Connection:
-    """Create a READ-ONLY database connection to a SQLite database"""
-    return sqlite3.connect(DB_URI, uri=True)
+def _create_connection(URI: str) -> sqlite3.Connection:
+    """Create a READ-ONLY database connection to an SQLite database"""
+    return sqlite3.connect(URI, uri=True)
 
 
 @tool
 def read_sql(query: str) -> str:
     """Run a read-only SELECT query against the Northwind Wholesale database."""
     try:
-        # Pass the Path object directly
-        connection = _create_connection()
+        connection = _create_connection(DB_URI_RO)
         cursor = connection.cursor()
 
         cursor.execute(query)
@@ -27,9 +27,55 @@ def read_sql(query: str) -> str:
         cursor.close()
         connection.close()
 
-        return json.dumps(rows)
+        return json.dumps({"status": "success",
+                           "message": "Query inserted successfully",
+                           "data": rows})
 
     except sqlite3.Error as e:
-        return json.dumps(f'Error occurred: {e}')
+        return json.dumps({"status": "success",
+                           "message": f"Error occurred: {e}",})
+
+@tool
+def insert_sql(query: str) -> str:
+    """Run an INSERT query to add a new Customer, Product, Supplier, etc."""
+    try:
+        connection = _create_connection(DB_URI_RW)
+        cursor = connection.cursor()
+
+        cursor.execute(query)
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return json.dumps({"status": "success",
+                           "message": "Query inserted successfully"})
+
+    except sqlite3.Error as e:
+        return json.dumps({"status": "success",
+                           "message": f"Error occurred: {e}", })
+
+@tool
+def inspect_schema() -> str:
+    """Inspect the schema of the Northwind Wholesale database.
+        Call this tool once to learn the schema and save it to your memory
+        so you don't have to call it again."""
+    try:
+        connection = _create_connection(DB_URI_RO)
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT name, sql FROM sqlite_master "
+                       "WHERE type='table' ORDER BY name")
+        rows = cursor.fetchall()
+        res = "\n\n".join([f"{row[0]}:\n{row[1]}" for row in rows])
+
+        cursor.close()
+        connection.close()
+
+        return res
+
+    except sqlite3.Error as e:
+        return json.dumps({"status": "success",
+                           "message": f"Error occurred: {e}", })
 
 
